0926520fe43de191cb1cb55205406238580884bb
Bruno Carvalho <bruno@brunocarvalho.me>
2016-04-26 22:05:48 -0300

    Initial commit

---
+++ dist/assets/js/scripts.js
@@ -0,0 +1,26 @@
+/*!
+ * mywedding_website
+ * My Wedding Website
+ * http://brukel.cf
+ * @author Bruno Carvalho
+ * @version 1.0.0
+ * Copyright 2016. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+	'use strict';
+	$(function () {
+
+		$(document).ready(function() {
+
+		});
+
+		$(window).resize(function() {
+
+		});
+
+		$(window).scroll(function() {
+
+		});
+
+	});
+})(jQuery, window, document);
---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+(function ($, window, document, undefined) {
+	'use strict';
+	$(function () {
+
+		$(document).ready(function() {
+
+		});
+
+		$(window).resize(function() {
+
+		});
+
+		$(window).scroll(function() {
+
+		});
+
+	});
+})(jQuery, window, document);
