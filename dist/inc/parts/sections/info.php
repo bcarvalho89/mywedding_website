fb18e8b6d07136212e349a987e55a47865cfa1e1
Bruno Carvalho <bruno@brunocarvalho.me>
2016-05-01 01:34:29 -0300

    Styling info section

--- dist/inc/parts/sections/info.php
+++ dist/inc/parts/sections/info.php
@@ -2,15 +2,48 @@
 <section id="info">
 	<?php title("Informações"); ?>
 
-	<div class="content text-center">
+	<div class="content">
 		<div class="container">
 
-		<div class="row">
-			<div class="col-xs-12">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor tempus justo, quis hendrerit nulla mattis sit amet. Vivamus ut sodales felis.</p>
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor tempus justo, quis hendrerit nulla mattis sit amet. Vivamus ut sodales felis.</p>
+				</div>
 			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<h3>Cerimônia e Recepção</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="info-wrap">
+						<div class="row middle-xs">
+							<div class="col-md-8">
+								<h4>Quando</h4>
+								<p>Domingo, 28 de Agosto de 2016 às 11:00h</p>
+								<br/>
+								<h4>Onde</h4>
+								<p>Espaço Monterrey, Tapera Grande, Itatiba/SP</p>
+							</div>
+							<div class="col-md-4">
+								<div class="qr">
+									<div class="img">
+										<img src="assets/img/qr_code_local.jpg" />
+									</div>
+									<div class="caption">
+										<span>Scaneie para abrir no Google Maps</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6" id="mapa">
+
+				</div>
 			</div>
 		</div>
-	</div>
 
-</section>
+	</section>
