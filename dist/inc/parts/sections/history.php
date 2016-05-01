2e8035d6c722c7f4bc1b2166ff2c2f24757fd1bb
Bruno Carvalho <bruno@brunocarvalho.me>
2016-05-01 01:14:13 -0300

    Styling history section

--- dist/inc/parts/sections/history.php
+++ dist/inc/parts/sections/history.php
@@ -5,11 +5,42 @@
 	<div class="content">
 		<div class="container">
 
-		<div class="row">
-			<div class="col-xs-12">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor tempus justo, quis hendrerit nulla mattis sit amet. Vivamus ut sodales felis. Maecenas sollicitudin tincidunt metus, a auctor tortor mollis eu. Quisque sed elit id mi lacinia maximus non eget magna. Phasellus vestibulum efficitur enim sit amet facilisis.</p>
-				<p>Duis cursus vehicula ornare. Aenean velit augue, dignissim id justo imperdiet, convallis molestie nisi. Sed elementum purus tellus, vitae cursus mauris lobortis ac. Cras non commodo nulla. Praesent eget pretium dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed fermentum, ligula ut elementum facilisis, risus sem ornare neque, nec laoreet metus velit id est. Pellentesque interdum, justo sed gravida bibendum, mi lectus placerat urna, et laoreet nisl leo eu libero.</p>
+			<div class="row">
+				<div class="off-md-2 col-md-4">
+					<div class="wedding">
+						<div class="photo raquel"></div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="wedding">
+						<div class="photo bruno"></div>
+					</div>
+				</div>
 			</div>
+
+			<div class="row middle-xs">
+				<div class="off-md-1 col-md-4 text-right">
+					<div class="raquel-text">
+						<p>A Noiva</p>
+						<h2>Raquel de Freitas Oliveira</h2>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<span class="and">&</span>
+				</div>
+				<div class="col-md-4">
+					<div class="bruno-text">
+					<p>O Noivo</p>
+						<h2>Bruno Gomes de Carvalho</h2>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor tempus justo, quis hendrerit nulla mattis sit amet. Vivamus ut sodales felis. Maecenas sollicitudin tincidunt metus, a auctor tortor mollis eu. Quisque sed elit id mi lacinia maximus non eget magna. Phasellus vestibulum efficitur enim sit amet facilisis.</p>
+					<p>Duis cursus vehicula ornare. Aenean velit augue, dignissim id justo imperdiet, convallis molestie nisi. Sed elementum purus tellus, vitae cursus mauris lobortis ac. Cras non commodo nulla. Praesent eget pretium dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed fermentum, ligula ut elementum facilisis, risus sem ornare neque, nec laoreet metus velit id est. Pellentesque interdum, justo sed gravida bibendum, mi lectus placerat urna, et laoreet nisl leo eu libero.</p>
+				</div>
 			</div>
 		</div>
 	</div>
