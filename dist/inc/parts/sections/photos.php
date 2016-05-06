0ea2289b7be559a5f2f73794509e3fa798fd0896
Bruno Carvalho <bruno@brunocarvalho.me>
2016-05-05 22:37:21 -0300

    Added a video for invitation

--- dist/inc/parts/sections/photos.php
+++ dist/inc/parts/sections/photos.php
@@ -14,30 +14,37 @@
 		</div>
 		<div class="container-fluid">
 
-			<div class="gallery">
-				<?php
-				$dir          = 'assets/img/gallery';
-				$file_display = array(
-					'jpg',
-					'jpeg',
-					'png',
-					'gif'
-					);
+			<div class="row middle-xs">
+				<div class="col-md-4">
+				<iframe width="100%" height="360" src="https://www.youtube.com/embed/6MmNsu9oOLk?rel=0&amp;controls=1" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-md-8">
+					<div class="gallery">
+						<?php
+						$dir          = 'assets/img/gallery';
+						$file_display = array(
+							'jpg',
+							'jpeg',
+							'png',
+							'gif'
+							);
 
-				if (file_exists($dir) == false) {
-					echo 'Directory \'', $dir, '\' not found!';
-				} else {
-					$dir_contents = scandir($dir);
+						if (file_exists($dir) == false) {
+							echo 'Directory \'', $dir, '\' not found!';
+						} else {
+							$dir_contents = scandir($dir);
 
-					foreach ($dir_contents as $file) {
-						$file_type = strtolower(end(explode('.', $file)));
+							foreach ($dir_contents as $file) {
+								$file_type = strtolower(end(explode('.', $file)));
 
-						if ($file !== '.' && $file !== '..' && in_array($file_type, $file_display) == true) {
-							echo '<a class="image" href="'. $dir. '/'. $file. '"><img src="'. $dir. '/'. $file. '" /></a>';
+								if ($file !== '.' && $file !== '..' && in_array($file_type, $file_display) == true) {
+									echo '<a class="image" href="'. $dir. '/'. $file. '"><img src="'. $dir. '/'. $file. '" /></a>';
+								}
+							}
 						}
-					}
-				}
-				?>
+						?>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
