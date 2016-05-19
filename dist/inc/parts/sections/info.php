a79e45155daa5cad5b8b4c0f3d8d6dc4f412c533
Bruno Carvalho <bruno@brunocarvalho.me>
2016-05-19 20:57:28 -0300

    Info section for mobile

--- dist/inc/parts/sections/info.php
+++ dist/inc/parts/sections/info.php
@@ -6,28 +6,28 @@
 		<div class="container">
 
 			<div class="row description">
-				<div class="off-xs-2 col-xs-8">
+				<div class="off-md-2 col-md-8 off-xs-1 col-xs-10">
 					<p>Fique informado em onde e quando será nosso casamento. Você não vai perder toda emoção desde o início, não é?</p>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-xs-12">
+				<div class="col-md-12 off-xs-1 col-xs-10">
 					<h3>Cerimônia e Recepção</h3>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-6 col-xs-12">
 					<div class="info-wrap">
-						<div class="row middle-xs">
-							<div class="col-md-8">
+						<div class="row middle-md">
+							<div class="col-md-8 col-xs-12">
 								<h4>Quando</h4>
 								<p>Domingo, 28 de Agosto de 2016 às 11:00h</p>
 								<br/>
 								<h4>Onde</h4>
 								<p>Espaço Monterrey, Tapera Grande<br/>Estrada Municipal Antônio Franchin, S/N Sítio Vila Mada, Itatiba/SP - <a href="https://www.google.com.br/maps/place/Espa%C3%A7o+Monterrey/@-23.0626751,-46.8725521,17z/data=!3m1!4b1!4m5!3m4!1s0x94cf297bb99b1b13:0xd84abc8f1bcd695e!8m2!3d-23.0626801!4d-46.8703634" target="_blank" class="link">Abrir mapa</a></p>
 							</div>
-							<div class="col-md-4">
+							<div class="col-md-4 col-xs-12">
 								<div class="qr">
 									<div class="img">
 										<img src="assets/img/qr_code_local.jpg" />
@@ -40,9 +40,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-md-6" id="mapa">
-
-				</div>
+				<div class="col-md-6 col-xs-12" id="mapa"></div>
 			</div>
 		</div>
 
