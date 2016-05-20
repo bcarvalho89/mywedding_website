bdfed03e774c984cdb5eb23285276ce9b9936b79
Bruno Carvalho <bruno@brunocarvalho.me>
2016-05-19 21:14:47 -0300

    Guestbook section for mobile

--- dist/inc/parts/sections/guestbook.php
+++ dist/inc/parts/sections/guestbook.php
@@ -5,7 +5,7 @@
 	<div class="content">
 		<div class="container">
 			<div class="row description">
-				<div class="off-xs-2 col-xs-8">
+				<div class="off-md-2 col-md-8 off-xs-1 col-xs-10">
 					<p>Utilize esse espaço para nos deixar uma mensagem de felicitações, votos ou qualquer outra coisa queira. Promotemos ler com muito carinho.</p>
 				</div>
 			</div>
@@ -16,7 +16,7 @@
 			<button id="load-more" class="btn pink">Carregar mais <i class="icon-plus"></i></button>
 
 			<div class="row">
-				<div class="off-md-3 col-md-6">
+				<div class="off-md-3 col-md-6 off-xs-1 col-xs-10">
 					<form id="guestbook_form">
 						<input type="text" name"txtName" id="guestName" placeholder="Nome" />
 						<textarea name="txtComment" id="guestMessage" placeholder="Mensagem"></textarea>
