8721adda355759f915f67ee7d02565a62cf9a624
Bruno Gomes de Carvalho <bruno@brunocarvalho.me>
2016-07-16 15:11:24 -0300

    Update config_sendmail.php

--- dist/put_this_in_other_webserver/config_sendmail.php
+++ dist/put_this_in_other_webserver/config_sendmail.php
@@ -3,7 +3,7 @@
 $mailConfig = array( 
 	'host' => 'mail.brunocarvalho.me',
 	'username' => 'rsvp@brunocarvalho.me',
-	'password' => 'PiIIzzr64Qno',
+	'password' => 'xxxxxxx',
 	'from_mail' => 'rsvp@brunocarvalho.me', 
 	'from_name' => 'Casamento Raquel e Bruno',
 	'subject' => 'Confirmação de presença',
